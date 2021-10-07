dcf5781c7383d7bad8c84ea356d2437705cded6b
JarvisMAC <nietzhuang@JarvisMac.local>
2021-10-07 23:13:06 +0800

    Added mux_lw.sv.

---
+++ Homework4/mux_lw.sv
@@ -0,0 +1,13 @@
+`include "define.sv"
+
+module mux_lw(
+  input         [`data_size-1:0]        a,
+  input         [`data_size-1:0]        b,
+  input                                 select,
+
+  output logic  [`data_size-1:0]        y
+);
+
+  assign y  = (!select)? a : b;
+
+endmodule
