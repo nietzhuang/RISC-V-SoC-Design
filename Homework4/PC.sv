f3de247720238f9af04bfdffc3d59eb2c706b0c5
JarvisMAC <nietzhuang@JarvisMac.local>
2021-10-07 20:13:07 +0800

    Added PC.sv.

--- Homework4/PC.sv
+++ Homework4/PC.sv
@@ -1,19 +1,19 @@
 `include "define.sv"
 
 module PC(
-  input [`data_size-1:0] PC_in_pdt,
-  input clk,
-  input rst,
-  input [6:0] opcode_EXE,
-  input [6:0] opcode_MEM,
-  input jump_sel,
-  input taken_sel,
-  input Istall,  // stall from cache
-  input Dstall,
-  input hit,
-    
-  output logic [`data_size-1:0] PC_address,
-  output logic Icache_en
+  input                                 clk,
+  input                                 rst,
+  input         [`data_size-1:0]        PC_in_pred,
+  input         [6:0]                   opcode_ID_EXE,
+  input         [6:0]                   opcode_EXE_MEM,
+  input                                 jump_sel,
+  input                                 taken_sel,
+  input                                 Istall,
+  input                                 Dstall,
+  input                                 hit,
+
+  output logic  [`data_size-1:0]        PC_address,
+  output logic                          Icache_en
 );
 
   always_ff@(posedge clk)begin
@@ -21,21 +21,12 @@ module PC(
       PC_address <= 32'h0fff_fffc;
       Icache_en <= 1'b0;
     end
-    else if((opcode_EXE == `Btype) && (opcode_MEM == `Stype) && ( (jump_sel && (!(taken_sel)) ) || ( (!(jump_sel)) && taken_sel ) ) )begin
-  // cosider the 1110 || 1101
-      if(!(Istall||Dstall))begin
+    else if(!(Istall||Dstall))begin
         PC_address <= PC_in_pdt;
         Icache_en <= 1'b1;
-      end
-      else
+    end
+    else
         Icache_en <= 1'b0;
-    end
-//    else if( (!(stall || stall_DM_en)) )begin
-    else if(!(Istall||Dstall))begin
-      PC_address <= PC_in_pdt;
-      Icache_en <= 1'b1;
-    end
-    else 
-      Icache_en <= 1'b0;
   end
+
 endmodule
