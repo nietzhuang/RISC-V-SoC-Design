f4d8a6aaa856d2f155cd1d28a1b116efa07ca6b5
JarvisMAC <nietzhuang@JarvisMac.local>
2021-10-08 18:20:44 +0800

    Added I_comparator.sv.

---
+++ Homework4/I_comparator.sv
@@ -0,0 +1,13 @@
+`include "define.sv"
+
+module I_comparator(
+  input     [`tag-1:0]      tag,
+  input     [`tag-1:0]      I_tag,
+  input                     v_bit,
+
+  output logic              hit
+);
+
+  assign ((tag==I_tag) && v_bit)? 1'b1 : 1'b0;
+
+endmodule
