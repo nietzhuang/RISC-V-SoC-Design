0eaf6c2aa8b5ec66d1ab64e56ba2c6d14a6b567a
JarvisMAC <nietzhuang@JarvisMac.local>
2021-10-08 16:01:13 +0800

    Added mux_sw_data.sv.

---
+++ Homework4/mux_sw_data.sv
@@ -0,0 +1,18 @@
+`include "define.sv"
+
+module mux_sw_data(
+  input         [`data_size-1:0]        Read_data_2_u,  // from mux_utype
+  input         [`data_size-1:0]        write_data,
+  input                                 sw_data_sel,
+
+  output logic  [`data_size-1:0]        Read_data_sw
+  );
+
+  always_comb begin
+    unique case(sw_data_sel)
+      1'b0: Read_data_sw = Read_data_2_a2;
+      1'b1: Read_data_sw = write_data;
+    endcase
+  end
+  
+endmodule
