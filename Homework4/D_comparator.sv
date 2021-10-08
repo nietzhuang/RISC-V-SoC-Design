e14c644e7019c608aa4d47e4c21895753b7bfcf6
JarvisMAC <nietzhuang@JarvisMac.local>
2021-10-08 19:24:46 +0800

    Added D_comparator.sv.

---
+++ Homework4/D_comparator.sv
@@ -0,0 +1,13 @@
+`include "define.sv"
+
+module D_comparator(
+  input     [`tag-1:0]      tag,
+  input     [`tag-1:0]      D_tag,
+  input                     v_bit,
+
+  output logic              hit
+);
+
+  assign hit = (tag == D_tag)? 1'b1 : 1'b0;
+
+endmodule
