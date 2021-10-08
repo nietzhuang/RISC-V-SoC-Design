b584f862541e53b5f0763a543f35c55e97057e00
JarvisMAC <nietzhuang@JarvisMac.local>
2021-10-08 15:59:19 +0800

    Added mux_rt.sv.

---
+++ Homework4/mux_rt.sv
@@ -0,0 +1,22 @@
+`include "define.sv"
+
+module mux_rt(
+  input         [`data_size-1:0]        imm_out,
+  input         [`data_size-1:0]        alu_result_EXE_MEM,
+  input         [`data_size-1:0]        write_data,
+  input         [`data_size-1:0]        Dcache_out_ext,
+  input         [1:0]                   rt_sel,
+
+  output logic  [`data_size-1:0]        src2
+);
+
+  always_comb begin
+    unique case(rt_sel)
+      2'b00: src2 = imm_out;
+      2'b01: src2 = alu_result_EXE_MEM;
+      2'b10: src2 = write_data;
+      2'b11: src2 = Dcache_out_ext;
+    endcase
+  end
+  
+endmodule
